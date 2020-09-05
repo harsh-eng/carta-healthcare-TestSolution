4cd8773028a004afe7e9cb9e62c30c098b0d2d52
Harshal Gaikwad <harshalg9@gmail.com>
2020-09-06 02:28:34 +0530

    Commit for carta-healthcare-problem statement solution

---
+++ rest-csv/src/main/java/healthcare/carta/interview/restcsv/controller/CsvController.java
@@ -0,0 +1,73 @@
+package healthcare.carta.interview.restcsv.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.minidev.json.JSONObject;
+
+@RestController
+public class CsvController {
+
+	@PostMapping(value = "/" , consumes = "multipart/form-data")
+	public ResponseEntity<Object> getAverage(@RequestParam ("data") MultipartFile  data, @RequestParam ("column") String column) {
+		String line="";
+		double sum=0;
+		double rows=0;
+		double outPut=0;
+		JSONObject obj = new JSONObject();
+		try {
+			InputStreamReader streamReader = new InputStreamReader(data.getInputStream(), StandardCharsets.UTF_8);
+			BufferedReader br = new BufferedReader(streamReader);
+			int index = -1;
+			if((line = br.readLine()) != null) {
+				String [] csvData =line.split(",");
+				for (int i=0;i<csvData.length;i++) {
+					if(column.equalsIgnoreCase(csvData[i])) {
+						index = i;
+						break;
+					}
+				}
+			}
+			if(index==-1) {
+				obj.put("Error", "Given column name not found");
+				return new ResponseEntity<> (obj,HttpStatus.BAD_REQUEST);
+			}
+
+			while ((line = br.readLine()) != null) {
+				String [] csvData =line.split(",");
+				sum = sum+Double.valueOf(csvData[index]);
+				rows++;
+
+			}
+			if(rows!=0) {
+				outPut=(sum/rows);
+			}
+		}catch(IOException e) {
+			obj.put("Error", e.getMessage());
+			return new ResponseEntity<> (obj,HttpStatus.BAD_REQUEST);
+		}
+		catch(NumberFormatException e) {
+			obj.put("Error","NumberFormat Exception:"+ e.getMessage());
+			return new ResponseEntity<> (obj,HttpStatus.BAD_REQUEST);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			obj.put("Error", e.getMessage());
+			return new ResponseEntity<> (obj,HttpStatus.BAD_REQUEST);
+		}
+
+		obj.put("data", outPut);
+		return new ResponseEntity<> (obj,HttpStatus.OK);
+
+	}
+
+}
